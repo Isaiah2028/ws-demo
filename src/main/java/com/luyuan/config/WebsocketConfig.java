fde2c948f94d78c0dcb5327c5d94ade00ac85ab7
luyuan <2244140752@qq.com>
2024-06-30 15:37:49 +0800

    文件换行格式改成mac格式

--- src/main/java/com/luyuan/config/WebsocketConfig.java
+++ src/main/java/com/luyuan/config/WebsocketConfig.java
@@ -1,15 +1,1 @@
-package com.luyuan.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.server.standard.ServerEndpointExporter;
-
-@Configuration
-public class WebsocketConfig {
-
-    @Bean
-    public ServerEndpointExporter serverEndpointExporter(){
-        return new ServerEndpointExporter();
-    }
-
-}
+package com.luyuan.config;import org.springframework.context.annotation.Bean;import org.springframework.context.annotation.Configuration;import org.springframework.web.socket.server.standard.ServerEndpointExporter;@Configurationpublic class WebsocketConfig {    @Bean    public ServerEndpointExporter serverEndpointExporter(){        return new ServerEndpointExporter();    }}
